b3a867359f38a3bf8e2dc1f1afa5194a2a86ca1d
Adokiye <gorgeceo@gmail.com>
2019-12-09 04:16:38 +0100

    Changes for Heroku

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    url: 'mongodb+srv://Aadokiye:adk20100@cluster0-mrw0d.mongodb.net/test?retryWrites=true&w=majority',
+    url: 'mongodb+srv://Adokiye:adk20100@cluster0-mrw0d.mongodb.net/test?retryWrites=true&w=majority',
     secret: 'gw_cleaners',
     square_up_access_token: 'EAAAEPUY37iB4hrXIDgwr8T3cO0rPP_x3hMA9eDGkSvzWmfhNOj39KnklohOtEhJ'
 }
