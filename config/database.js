66f4200c732ea30e866ffe83130bd1f9f4542474
Adokiye <gorgeceo@gmail.com>
2019-12-09 04:14:33 +0100

    Addition of mlab config

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    url: 'mongodb://localhost/mongolab-vertical-20727',
+    url: 'mongodb+srv://Aadokiye:adk20100@cluster0-mrw0d.mongodb.net/test?retryWrites=true&w=majority',
     secret: 'gw_cleaners',
     square_up_access_token: 'EAAAEPUY37iB4hrXIDgwr8T3cO0rPP_x3hMA9eDGkSvzWmfhNOj39KnklohOtEhJ'
 }
