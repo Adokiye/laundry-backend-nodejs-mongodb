af2a1297b6238219bfb6484ec2a4314726b22014
Adokiye <gorgeceo@gmail.com>
2019-12-09 04:01:10 +0100

    Changes for Heroku

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/gw-cleaners',
+    url: 'mongodb://localhost/mongolab-vertical-20727',
     secret: 'gw_cleaners',
     square_up_access_token: 'EAAAEPUY37iB4hrXIDgwr8T3cO0rPP_x3hMA9eDGkSvzWmfhNOj39KnklohOtEhJ'
 }
