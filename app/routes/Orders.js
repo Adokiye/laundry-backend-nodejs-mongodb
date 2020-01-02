847b8b2aa4b2cd538f1255474cbc18a7ce9aab43
Adokiye <gorgeceo@gmail.com>
2020-01-02 01:39:02 +0100

    Route addition for creating Order

--- app/routes/Orders.js
+++ app/routes/Orders.js
@@ -4,6 +4,8 @@ module.exports = (app) => {
     // Create a new Order
     app.post('/orders', middleware.checkToken, orders.create);
 
+    app.post('/orders/adminCharge', middleware.checkToken, orders.adminEditOrder);
+
     // Retrieve all Orders
     app.get('/orders/:userId', middleware.checkToken, orders.findAll);
 
@@ -15,6 +17,8 @@ module.exports = (app) => {
 
     // Update a Order with orderId
     app.put('/orders/:orderId', middleware.checkToken, orders.updateStage);
+
+
 
     // Delete a Order with orderId
     app.delete('/orders/:orderId', middleware.checkToken, orders.delete);
