6fb4c79abfc0efe258fd048ddff42e858b6778d1
Adokiye <gorgeceo@gmail.com>
2020-01-04 22:58:29 +0100

    Fix square up

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    url:  'mongodb://Adokiye:adk20100@ds253368.mlab.com:53368/heroku_9gf28k70',  //'mongodb://localhost:27017/gw-cleaners',         
+    url:  'mongodb://Adokiye:adk20100@ds253368.mlab.com:53368/heroku_9gf28k70',   //'mongodb://localhost:27017/gw-cleaners', \        
     secret: 'gw_cleaners',
     square_up_access_token: 'EAAAEPUY37iB4hrXIDgwr8T3cO0rPP_x3hMA9eDGkSvzWmfhNOj39KnklohOtEhJ'
 }
