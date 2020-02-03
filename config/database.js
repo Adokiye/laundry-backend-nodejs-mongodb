97ee8d0390794c6797961f198b388b6a671edcd6
Adokiye <38386612+Adokiye@users.noreply.github.com>
2020-02-03 10:14:48 +0100

    fix

---
+++ config/database.js
@@ -0,0 +1,8 @@
+module.exports = {
+    url:  'mongodb://localhost:27017/gw-cleaners',  
+    secret: 'gw_cleaners',
+    square_up_access_token: 'EAAAEPUY37iB4hrXIDgwr8T3cO0rPP_x3hMA9eDGkSvzWmfhNOj39KnklohOtEhJ',
+    stripe_key: 'sk_test_3MEBqlPa7OW7mx6xw4QXl3tV00eueSCM51',
+    email_pass: 'femi123$',
+    email:'washnbox@gmail.cm'
+}
